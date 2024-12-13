3a504e8f8b45b9b7870b762a7e23cb934f4a9e29
Chefminh <166143843+Chefminh@users.noreply.github.com>
2024-12-13 23:01:21 +0700

    Linear system.cpp

--- Linear system.cpp
+++ Linear system.cpp
@@ -40,11 +40,41 @@ void LinearSystem::inputSystem()
 // Solving the linear system
 void LinearSystem::LS_logic()
 {
-    if (A.empty() || B.empty())
+    vector <double> tempB;
+    vector<vector<double>> tempA;
+
+
+
+    int n = matrix.columns;
+    bool fillAll = true;
+    for (int j = 0; j < n; j++) {
+        vector<double> v;
+        for (int i = 0; i < n; i++) {
+            if (!box[i][j].numberEntered) {
+                fillAll = false;
+            }
+            v.push_back(box[i][j].numbersArray);
+        }
+        tempA.push_back(v);
+    }
+
+    for (int i = 0;i < n; i++) {
+        if (!linearValue[0][i].numberEntered) {
+            fillAll = false;
+        }
+        tempB.push_back(linearValue[0][i].numbersArray);
+    }
+    if (!fillAll || n == 0)
     {
         cout << "Please provide valid inputs." << endl;
         return;
     }
+
+    A.clear();
+    B.clear();
+    solution.clear();
+    A = tempA;
+    B = tempB;
 
     // Ensure the right size of matrix
     if (A.size() != A[0].size() || B.size() != A.size())
@@ -54,7 +84,6 @@ void LinearSystem::LS_logic()
     }
 
     // Number of equations
-    int n = A.size();
 
     // Augmented Matrix [ A | B ]
     vector<vector<double>> augmented(n, vector<double>(n + 1));
@@ -122,19 +151,19 @@ void LinearSystem::draw()
     float cell_width = 100, cell_height = 50;
     float start_x = 100, start_y = 100;
 
-    // Draw the coefficient matrix A
-    for (int i = 0; i < A.size(); ++i) {
-        for (int j = 0; j < A[i].size(); ++j) {
-            Rectangle rect = { start_x + offset_x + j * cell_width, start_y + offset_y + i * cell_height, cell_width, cell_height };
-            DrawRectangleRounded(rect, 0.2, 4, LIGHTGRAY);
-            DrawText(TextFormat("%.2lf", A[i][j]), rect.x + 15, rect.y + 15, 20, BLACK);
-        }
+    //// Draw the coefficient matrix A
+    //for (int i = 0; i < A.size(); ++i) {
+    //    for (int j = 0; j < A[i].size(); ++j) {
+    //        Rectangle rect = { start_x + offset_x + j * cell_width, start_y + offset_y + i * cell_height, cell_width, cell_height };
+    //        DrawRectangleRounded(rect, 0.2, 4, LIGHTGRAY);
+    //        DrawText(TextFormat("%.2lf", A[i][j]), rect.x + 15, rect.y + 15, 20, BLACK);
+    //    }
 
-        // Draw the constants column B
-        Rectangle rectB = { start_x + offset_x + A[i].size() * cell_width + 50, start_y + offset_y + i * cell_height, cell_width, cell_height };
-        DrawRectangleRounded(rectB, 0.2, 4, LIGHTGRAY);
-        DrawText(TextFormat("%.2lf", B[i]), rectB.x + 15, rectB.y + 15, 20, BLACK);
-    }
+    //    // Draw the constants column B
+    //    Rectangle rectB = { start_x + offset_x + A[i].size() * cell_width + 50, start_y + offset_y + i * cell_height, cell_width, cell_height };
+    //    DrawRectangleRounded(rectB, 0.2, 4, LIGHTGRAY);
+    //    DrawText(TextFormat("%.2lf", B[i]), rectB.x + 15, rectB.y + 15, 20, BLACK);
+    //}
 
     // Draw solution if available
     if (!solution.empty()) {
@@ -142,7 +171,7 @@ void LinearSystem::draw()
         float solution_start_y = start_y + offset_y + (A.size() + 1) * cell_height;
 
         for (int i = 0; i < solution.size(); ++i) {
-            Rectangle rectSolution = { solution_start_x + i * cell_width, solution_start_y, cell_width, cell_height };
+            Rectangle rectSolution = { solution_start_x + i * cell_width + 20 * i, solution_start_y + 20, cell_width, cell_height };
             DrawRectangleRounded(rectSolution, 0.2, 4, LIGHTGRAY);
             DrawText(TextFormat("x%d = %.2lf", (int)i + 1, solution[i]), rectSolution.x + 10, rectSolution.y + 15, 20, BLACK);
         }
